bc40a22973ab0403252c824c3cba5275201d63dd
Andriy Kovalov <andy.kovv@gmail.com>
2018-10-17 23:57:41 +0300

    Add read config from file to postgres

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,64 @@
+/// Config module uses for create config for different connections
+/// Get configs from for coonectio from here
+/// If need update or add new config - add/update tests, add/update code
+/// Tests need to be up to date 
 
+
+use std::env;
+use std::path::{Path};
+use dotenv::from_path;
+use std::collections::HashMap;
+use test_utils::files_utils::FileBuilder;
+const ENV_FILE_VAR: &'static str = "config_file";
 
 pub struct Config;
 
 impl Config {
     /// Get_postgress_connection_url() get database connection url from config
     /// This method uses only for postgresql connection.
+    pub fn parse_env_vars() -> HashMap<String, String> {
+        // Method should patse env variables and add it to hash map
+        let mut env_variables = HashMap::new();
+        for (key, value) in env::vars() {
+            env_variables.insert(key, value);
+        }
+        env_variables
+    }
+
+    pub fn check_env_var(key: &str) -> String {
+        // Method should check if env var exist
+        // Think! need raise error if not found, or add flag with param for panic
+        match env::var(key) {
+            Ok(val) => val.to_string(),
+            Err(_) => {
+                error!("Env variable not found {}", key);
+                panic!();
+            }
+        }
+    }
+
     pub fn get_postgress_connection_url() -> String {
         // Dummy hardcoded url
         // connect config later
-        "postgres://fjam:fjam@localhost/fjam".to_string()
+        let config = Config::parse_env_vars();
+        let conf_file_path = match config.get(ENV_FILE_VAR) {
+            Some(conf_file) => {
+                info!("Config path is set, path: {}", &conf_file);
+                conf_file
+            },
+            None => {
+                error!("Config path is not set, please set env variable {}", ENV_FILE_VAR);
+                panic!();
+            },
+        };
+        let path = Path::new(conf_file_path);
+        from_path(path).ok();
+
+        let check_env_var = Config::check_env_var;
+        let user = check_env_var("POSTGRES_USER");
+        let pass = check_env_var("POSTGRES_PASS");
+        let db = check_env_var("POSTGRES_DATABASE");
+        format!("postgres://{}:{}@localhost/{}",user, pass, db )
     }
 }
 
@@ -18,14 +68,19 @@ mod config_tests {
     use super::*;
 
     #[test]
-    fn test_should_return_postgresql_config() {
+    fn test_should_create_postgres_url_from_created_file() {
+        let data = b"POSTGRES_USER=fjam \n POSTGRES_PASS=fjam \n POSTGRES_DATABASE=fjam";
+        let created_file = 
+            FileBuilder::new()
+            .create_tmp_dir("temp")
+            .file_name("env")
+            .add_data(data)
+            .build()
+            .create();
+        env::set_var("config_file", created_file.get_file_path());
         let postgres_url = Config::get_postgress_connection_url();
         let expected_url = "postgres://fjam:fjam@localhost/fjam";
+        created_file.remove_with_dir();
         assert_eq!(postgres_url, expected_url);
-    }
-
-    #[test]
-    fn test_should_create_postgres_url_from_created_file() {
-        
     }
 }
--- src/test_utils/files_utils.rs
+++ src/test_utils/files_utils.rs
@@ -2,14 +2,13 @@
 /// When you need create or remove different files for unittesting or integratio tests
 /// simple use structs wich implemented in this file
 
-use std::panic::catch_unwind;
-use std::fs::File as SourceFile;
+use std::fs::{File, remove_dir_all};
+use std::io::prelude::*;
 use std::fs::create_dir_all;
-use std::path::Path;
 use std::env::current_dir;
 
 #[derive(Debug)]
-pub struct File {
+pub struct MockFile {
     name: String,
     dir_name: String,
     extension: Option<String>,
@@ -17,11 +16,11 @@ pub struct File {
     path: String,
 }
 
-/// Implementation File methods
+/// Implementation MockFile methods
 /// create -> create file with choosen params that was craeted by builder
-impl File {
+impl MockFile {
 
-    fn get_file_path(&self) -> String {
+    pub fn get_file_path(&self) -> String {
         // Method should return concated file path
         match &self.extension {
             Some(extension) => format!("{}/{}/{}.{}", self.path, self.dir_name, self.name, extension),
@@ -29,7 +28,12 @@ impl File {
         }
     }
 
-    fn create_tmp_dir(&self) {
+    pub fn get_abs_path(&self) -> String {
+        // Method should get absolut path for directory
+        format!("{}/{}", self.path, self.dir_name)
+    }
+
+    pub fn create_tmp_dir(&self) {
         // Method should create tmp dir
         // If error raise panic!
         let path_to_dir = format!("{}/{}", self.path, self.dir_name);
@@ -40,13 +44,29 @@ impl File {
         };
     }
 
-    fn create(self) -> Self {
+    pub fn remove_with_dir(&self) -> &Self {
+        // Method should remove files with its dir
+        let path = self.get_abs_path();
+        match remove_dir_all(&path) {
+            Ok(_) => info!("Directory removed successully {}", path),
+            Err(err) => {
+                error!(
+                    "Directory removed failure {}, err: {:?}", path, err
+                )}, 
+        };
+        self
+    }
+
+    pub fn create(self) -> Self {
         // Method should create file with income params
         self.create_tmp_dir();
         let file_path = self.get_file_path();
-        let file = SourceFile::create(file_path);
+        let file = File::create(file_path);
         match file {
-            Ok(data) => println!("{:?}", data),
+            Ok(mut file) => {
+                file.write_all(&self.data).unwrap();
+                println!("Wrote data is {:?}", &self.data);
+            },
             Err(err) => eprintln!("Error create file {:?}", err),
         };
         self
@@ -82,7 +102,7 @@ impl Default for FileBuilder {
             _name: "default".to_string(),
             _dir_name: "tmp".to_string(),
             _extension: None,
-            _data: vec![1],
+            _data: vec![],
             _path: path,
         }
     }
@@ -100,32 +120,38 @@ impl FileBuilder {
         Self{..Default::default()}
     }
 
-    fn create_tmp_dir<S: Into<String>>(mut self, name: S) -> Self {
+    pub fn create_tmp_dir<S: Into<String>>(mut self, name: S) -> Self {
         // Method create temp dir with chosen name
         self._dir_name = name.into();
         self
     }
 
-    fn file_name<S: Into<String>>(mut self, name: S) -> Self {
+    pub fn file_name<S: Into<String>>(mut self, name: S) -> Self {
         // Method should add name for file
         self._name = name.into();
         self
     }
 
-    fn extension<S: Into<String>>(mut self, extension: S) -> Self {
+    pub fn extension<S: Into<String>>(mut self, extension: S) -> Self {
         // Method should create extension for file
         self._extension = Some(extension.into());
         self
     }
 
-    fn build(&self) -> File {
+    pub fn add_data(mut self, data:  &[u8]) -> Self {
+        // Method should add data to bytes vector
+        self._data.append(&mut data.to_vec());
+        self
+    }
+
+    pub fn build(&self) -> MockFile {
         // Method should build data and return File instance
-        File {
+        MockFile {
             name: self._name.clone(),
             dir_name: self._dir_name.clone(),
             extension: self._extension.clone(),
             // TODO add data method
-            data: vec![1],
+            data: self._data.clone(),
             path: self._path.clone(),
         }
     }
@@ -133,7 +159,7 @@ impl FileBuilder {
 
 
 #[cfg(test)]
-pub mod file_integration {
+pub mod file_integration_tests {
     use super::*;
 
     #[test]
@@ -149,7 +175,43 @@ pub mod file_integration {
         println!("{:?}", created_file);
         assert_eq!(created_file.name, "test");
         assert_eq!(created_file.extension.unwrap(), "txt");
-
         assert_eq!(created_file.path, current_dir.display().to_string());
+    }
+
+    #[test]
+    fn test_should_add_data_to_created_file() {
+        let expected_byte_string = b"some bytes data";
+        let created_file = 
+            FileBuilder::new()
+            .file_name("file_with_data")
+            .add_data(expected_byte_string)
+            .build()
+            .create();
+        let file_abs_path = created_file.get_file_path();
+        let mut file = File::open(file_abs_path).expect("File not found");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        assert_eq!(contents, String::from_utf8(expected_byte_string.to_vec()).unwrap()); 
+    }
+
+    #[test]
+    fn test_should_remove_directory_with_files_after_remove_with_dir_call() {
+        let created_file = 
+            FileBuilder::new()
+            .file_name("file_with_data")
+            .build()
+            .create();
+
+        let exist_file = match File::open(created_file.get_file_path()) {
+            Ok(_) => true,
+            Err(_) => false,
+        };
+        assert_eq!(exist_file, true);
+        created_file.remove_with_dir();
+        let non_exist_file = match File::open(created_file.get_file_path()) {
+            Ok(_) => true,
+            Err(_) => false,
+        };
+        assert_eq!(non_exist_file, false);
     }
 }
